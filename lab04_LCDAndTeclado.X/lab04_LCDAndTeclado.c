3105ea40d632150d7875a78faab0d3ea47a8ca33
esteban11519 <eladinof@unal.edu.co>
2023-05-15 17:58:07 -0500

    Working with keypad interruption

--- lab04_LCDAndTeclado.X/lab04_LCDAndTeclado.c
+++ lab04_LCDAndTeclado.X/lab04_LCDAndTeclado.c
@@ -9,10 +9,13 @@
 #pragma config PBADEN=OFF	/* RB0 to RB4 as digital i/o */
 #pragma config LVP=OFF		/* RB5 as digital i/o */
 
-unsigned char LeerTeclado(void);
+// Global variables
+unsigned char Tecla=0;
+
+// Interruption Service Rutine
+void __interrupt() ISR(void);
+
 void main(void){
-  /* Variables */
-  unsigned char Tecla=0;
   
   /* LCD configuration */
   TRISD=0;			/*PortD as output */
@@ -27,6 +30,10 @@ void main(void){
   LATB=0b00000000;	     /* RB3:RB0 in 0 logic */
   RBPU=0;			/*  PORTB Pull-up Enable bit, 0 is enable pullup resistence */
 
+  RBIF=0; 			// RB Port Change Interrupt Flag bit, 0:  None of the RB7:RB4 pins have changed state
+  RBIE=1;			// RB Port Change Interrupt Enable bit, 1: Enables the RB port change interrupt
+  GIE=1;  			// Global Interrupt Enable bit, 1: = Enables all unmasked interrupts
+  
   /* General configuration */
   __delay_ms(1000);		/* LCD stabilize time (1000 ms) and pull-up resistences (100 ms)*/
 
@@ -40,44 +47,53 @@ void main(void){
   BorraLCD();
   
   while(1){
-    Tecla=LeerTeclado();
-    if(Tecla!=0){
-      EscribeLCD_n8(Tecla,2);
-      __delay_ms(1000); 
-      Tecla=0;
-      BorraLCD();
-    }
-      
+    EscribeLCD_n8(Tecla,2);
+    SLEEP();
+    BorraLCD();
   }    
 }
 
+void __interrupt() ISR(void){
+  unsigned char aux_PORTB=PORTB;
 
-unsigned char LeerTeclado(void){
-
-  LATB=0b11111110;
-  __delay_ms(10); 		/* Delay to stabilize */
-  if(RB4==0) return 1;
-  if(RB5==0) return 2;
-  if(RB6==0) return 3;
-  if(RB7==0) return 4;
-  LATB=0b11111101;
-  __delay_ms(10);
-  if(RB4==0) return 5;
-  if(RB5==0) return 6;
-  if(RB6==0) return 7;
-  if(RB7==0) return 8;
-  LATB=0b11111011;
-  __delay_ms(10);
-  if(RB4==0) return 9;
-  if(RB5==0) return 10;
-  if(RB6==0) return 11;
-  if(RB7==0) return 12;
-  LATB=0b11110111;
-  __delay_ms(10);
-  if(RB4==0) return 13;
-  if(RB5==0) return 14;
-  if(RB6==0) return 15;
-  if(RB7==0) return 16;
-
-  return 0;
+  if(RBIF==1){
+    // Falling edge discrimination
+    if((aux_PORTB & 0b11110000)!=0b11110000){
+      Tecla=0;
+      LATB=0b11111110;
+       __delay_ms(10);
+      if(RB4==0) Tecla=1;
+      else if(RB5==0) Tecla=2;
+      else if(RB6==0) Tecla=3;
+      else if(RB7==0) Tecla=4;
+      else{
+	LATB=0b11111101;
+	 __delay_ms(10);
+	if(RB4==0) Tecla=5;
+	else if(RB5==0) Tecla=6;
+	else if(RB6==0) Tecla=7;
+	else if(RB7==0) Tecla=8;
+	else{
+	  LATB=0b11111011;
+	   __delay_ms(10);
+	  if(RB4==0) Tecla=9;
+	  else if(RB5==0) Tecla=10;
+	  else if(RB6==0) Tecla=11;
+	  else if(RB7==0) Tecla=12;
+	  else{
+	    LATB=0b11110111;
+	     __delay_ms(10);
+	    if(RB4==0) Tecla=13;
+	    else if(RB5==0) Tecla=14;
+	    else if(RB6==0) Tecla=15;
+	    else if(RB7==0) Tecla=16;
+	  }
+	}
+      }
+      LATB=0b11110000;
+    }
+    __delay_ms(100);
+    RBIF=0;
+  }
+  return;
 }
