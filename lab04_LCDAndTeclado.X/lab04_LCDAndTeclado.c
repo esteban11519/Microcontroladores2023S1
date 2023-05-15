27ddcb2fc896d9ce32d5634109823926a515cde0
esteban11519 <eladinof@unal.edu.co>
2023-05-15 04:25:54 -0500

    Se puede leer el teclado de manera extraña

--- lab04_LCDAndTeclado.X/lab04_LCDAndTeclado.c
+++ lab04_LCDAndTeclado.X/lab04_LCDAndTeclado.c
@@ -1,18 +1,83 @@
+/*
+  This code is adapted from Blanco Robin proffesor codes.
+*/
 #include<xc.h>
-#define _XTAL_FREQ 1000000
+#define _XTAL_FREQ 1000000 // Fosc = 1MHz; define before of LibLCDXC8.h
 #include "LibLCDXC8.h"
-#pragma config FOSC=INTOSC_EC
-#pragma config WDT=OFF
+#pragma config FOSC=INTOSC_EC 	/*Internal oscillator 1 MHz */
+#pragma config WDT=OFF		/* Watchdog off  */
+#pragma config PBADEN=OFF	/* RB0 to RB4 as digital i/o */
+#pragma config LVP=OFF		/* RB5 as digital i/o */
+
+unsigned char LeerTeclado(void);
 void main(void){
-  TRISD=0;
-  LATD=0;
-  __delay_ms(1000);
+  /* Variables */
+  unsigned char Tecla=0;
+  
+  /* LCD configuration */
+  TRISD=0;			/*PortD as output */
+  LATD=0;			/* PortD inir in 0 logic */
+  
   ConfiguraLCD(4);
   InicializaLCD();
+
+  
+  /* Keyboard configuration */
+  TRISB=0b11110000;	     /*  RB7:RB4 as input and RB3:RB0 as output */
+  LATB=0b00000000;	     /* RB3:RB0 in 0 logic */
+  RBPU=0;			/*  PORTB Pull-up Enable bit, 0 is enable pullup resistence */
+
+  /* General configuration */
+  __delay_ms(1000);		/* LCD stabilize time (1000 ms) and pull-up resistences (100 ms)*/
+
+
+  /* Welcome message */
   MensajeLCD_Var("Hola Mundo");
   DireccionaLCD(0xC0); 
   MensajeLCD_Var("Dios es bueno");
 
+  __delay_ms(1000);
+  BorraLCD();
+  
   while(1){
+    Tecla=LeerTeclado();
+    if(Tecla!=0){
+      EscribeLCD_n8(Tecla,2);
+      __delay_ms(1000); 
+      Tecla=0;
+      BorraLCD();
+    }
+      
   }    
+}
+
+
+unsigned char LeerTeclado(void){
+
+  LATB=0b11111110;
+  __delay_ms(10); 		/* Delay to stabilize */
+  if(RB4==0) return 1;
+  if(RB5==0) return 2;
+  if(RB6==0) return 3;
+  if(RB7==0) return 4;
+  LATB=0b11111101;
+  __delay_ms(10);
+  if(RB4==0) return 5;
+  if(RB5==0) return 6;
+  if(RB6==0) return 7;
+  if(RB7==0) return 8;
+  LATB=0b11111011;
+  __delay_ms(10);
+  if(RB4==0) return 9;
+  if(RB5==0) return 10;
+  if(RB6==0) return 11;
+  if(RB7==0) return 12;
+  LATB=0b11110111;
+  __delay_ms(10);
+  if(RB4==0) return 13;
+  if(RB5==0) return 14;
+  if(RB6==0) return 15;
+  if(RB7==0) return 16;
+
+  return 0;
 }
