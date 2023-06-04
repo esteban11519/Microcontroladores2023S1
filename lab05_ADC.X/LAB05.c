b798d234fbe959a6747f71ef41a84e1fef55490d
esteban11519 <eladinof@unal.edu.co>
2023-06-04 02:32:25 -0500

    all worked in simulation

--- lab05_ADC.X/LAB05.c
+++ lab05_ADC.X/LAB05.c
@@ -13,9 +13,9 @@
 #pragma config WDTPS = 512 // T = 1/(31kHz/(128*WDTPS)) (2,11 s), [2^0, 2^13] discrete
 
 // DHT11 pines
-#define DATA_DIR TRISC0
-#define DATA_IN RC0
-#define DATA_OUT LATC0
+#define Data_Dir TRISA4
+#define Data_In RA4
+#define Data_Out LATA4
 
 // RGB pines
 #ifndef RGB_R
@@ -51,23 +51,30 @@
 
 unsigned char Temp,Hum,Che;
 unsigned char Enable_sample = 0;
+unsigned char Scale_Temperature = 0;
 
 // Functions
 // To DHT11
-void LeerHT11(void);
-unsigned char LeerByte(void);
-unsigned char LeerBit(void);
-unsigned char Check(void);
+void DHT11_Start(void);
+void DHT11_CheckResponse(void);
+char DHT11_ReadData(void);
+
+//RS232
 void Transmitir (unsigned char);
 
+//EEPROM
+void EEPROM_Write (unsigned char, unsigned char);
+unsigned char EEPROM_Read (unsigned char);
+
+// Main functions
 void init_configuration(void);	// [x] Init configuration
 void welcome_operations(void);  // [x]
-
+void measure_temperature_c_humidity_per(void); // [x] Temperature in celsius and Humidity in percent
 void send_temperature_RS232(void); // [x]
 void save_temperature_c_EEPROM(void); // []
 void choose_scale_temperature_and_fill_buffers(unsigned char *, unsigned char *); // [x]
 void show_temperature_c_RGB(void); // [x]
-void show_temperature_LCD(unsigned char *, unsigned char *, unsigned char, unsigned char); // [x]
+void show_temperature_LCD(unsigned char *, unsigned char *, unsigned char, unsigned char, unsigned char); // [x]
 void check_potentiometer_voltaje(void); // [x]
 
 
@@ -91,29 +98,30 @@ void main(void) {
   
   unsigned char Temp_n1=255; // Previous Temp
   unsigned char Hum_n1=255; // Previous Hum
+  unsigned char Scale_Temperature_n1=255; // Previous Scale Temperature 
   
   while(1){
     
     if(Enable_sample){
 
-      Temp++;
-      if(Temp > 50) Temp = 18;
-      Hum+=10;
-      
-      //LeerHT11();
-      CLRWDT(); // Clear Watchdog Timer
-      
+      // Temp++;
+      // if(Temp > 50) Temp = 18;
+      // Hum+=10;
+      measure_temperature_c_humidity_per();
+      CLRWDT(); // Clear Watchdog Timer      
       send_temperature_RS232();
       save_temperature_c_EEPROM();
       choose_scale_temperature_and_fill_buffers(buffer_LCD_row_1,buffer_LCD_row_2);
       show_temperature_c_RGB();
-      show_temperature_LCD(buffer_LCD_row_1, buffer_LCD_row_2, Temp_n1, Hum_n1);
+      show_temperature_LCD(buffer_LCD_row_1, buffer_LCD_row_2, Temp_n1, Hum_n1, Scale_Temperature_n1);
       check_potentiometer_voltaje();
       Enable_sample = 0;
 
       // Update Temp_n1 and Hum_n1
       Temp_n1=Temp;
       Hum_n1=Hum;
+      Scale_Temperature_n1=Scale_Temperature;
+
     }
     
   }
@@ -128,7 +136,6 @@ void init_configuration(void){
   __delay_ms(1);
 
   // DHT11
-  DATA_OUT=0;
   
   // RS232 
   TXSTA=0b00100000;
@@ -195,6 +202,7 @@ void init_configuration(void){
 void welcome_operations(void){
   /* Welcome message */
 
+  // When Reset is give WatchDog Timer
   if(TO==0){
     TO=1;
     MensajeLCD_Var("Failed DHT11");
@@ -205,12 +213,68 @@ void welcome_operations(void){
   MensajeLCD_Var("Sensors");
   MensajeLCD_Var("\nGod is good");
   __delay_ms(1000);
-  BorraLCD();
 
-   
+  // Show last temperature storaged in EEPROM
+  BorraLCD();
+  MensajeLCD_Var("Last Tempetature");
+  MensajeLCD_Var("\nT: ");
+  EscribeLCD_n8(EEPROM_Read(0x0),2);
+  MensajeLCD_Var(" C");
+  __delay_ms(1000);
+ 
   return;
 }
 
+void measure_temperature_c_humidity_per(void){
+  DHT11_Start();  			/* send start pulse to DHT11 module */
+  DHT11_CheckResponse();  	/* wait for response from DHT11 module */
+
+  /* read 40-bit data from DHT11 module */
+  Hum = DHT11_ReadData();  /* read Relative Humidity's integral value */
+  DHT11_ReadData();   /* read Relative Humidity's decimal value */
+  Temp = DHT11_ReadData();   /* read Temperature's integral value */
+  DHT11_ReadData();    /* read Relative Temperature's decimal value */
+  DHT11_ReadData();     /* read 8-bit checksum value */
+    	
+  return;
+}
+
+
+unsigned char DHT11_ReadData()
+{
+  unsigned char i,data = 0;  
+    for(i=0;i<8;i++)
+    {
+        while(!(Data_In & 1));  /* wait till 0 pulse, this is start of data pulse */
+        __delay_us(30);         
+        if(Data_In & 1)  /* check whether data is 1 or 0 */    
+          data = ((data<<1) | 1); 
+        else
+          data = (data<<1);  
+        while(Data_In & 1);
+    }
+  return data;
+}
+
+void DHT11_Start()
+{    
+    Data_Dir = 0;  /* set as output port */
+    Data_Out = 0;  /* send low pulse of min. 18 ms width */
+    __delay_ms(18);
+    Data_Out = 1;  /* pull data bus high */
+    __delay_us(20);
+    Data_Dir = 1;  /* set as input port */
+    Data_Out = 0;  /* Data Out in cero */
+    return;
+}
+
+void DHT11_CheckResponse()
+{
+    while(Data_In & 1);  /* wait till bus is High */     
+    while(!(Data_In & 1));  /* wait till bus is Low */
+    while(Data_In & 1);  /* wait till bus is High */
+    return;
+}
 
 void check_potentiometer_voltaje(void){
   ADON=1;
@@ -223,7 +287,9 @@ void check_potentiometer_voltaje(void){
   ADON=0;
   return;
 }
+
 void save_temperature_c_EEPROM(void){
+  EEPROM_Write(0x0, Temp);
   return;
 }
 void show_temperature_c_RGB(void){
@@ -283,32 +349,42 @@ void show_temperature_c_RGB(void){
 
 void choose_scale_temperature_and_fill_buffers(unsigned char *buffer_LCD_row_1, unsigned char *buffer_LCD_row_2){
   int temp_converted = 0;
-  
+  //Prevent switch bounce
+  __delay_ms(10);
   if(INT_1==0 && INT_2==0){
     // Celsius to celsius
-   
     sprintf(buffer_LCD_row_1, "T: %i C",Temp);
     sprintf(buffer_LCD_row_2, "H: %i %c",Hum,37);
+
+    Scale_Temperature=0;
   }
   else if(INT_1==0 && INT_2==1){
     // Celsius to Kelvin
     temp_converted = Temp + 273.15;
+
     sprintf(buffer_LCD_row_1, "T: %i K",temp_converted);
     sprintf(buffer_LCD_row_2, "H: %i %c",Hum,37);
+
+    Scale_Temperature=1;
   }
   else if(INT_1==1 && INT_2==0){
     // Celsius to Rankine
     temp_converted = Temp*1.8 + 491.67;
-     sprintf(buffer_LCD_row_1, "T: %i Ra",temp_converted);
+
+    sprintf(buffer_LCD_row_1, "T: %i Ra",temp_converted);
     sprintf(buffer_LCD_row_2, "H: %i %c",Hum,37);
+
+    Scale_Temperature=2;
     
   }
   else if(INT_1==1 && INT_2==1){
     // Celsius to Fahrenheit
     temp_converted = Temp*9.0/5.0 + 32.0;
 
-     sprintf(buffer_LCD_row_1, "T: %i F",temp_converted);
+    sprintf(buffer_LCD_row_1, "T: %i F",temp_converted);
     sprintf(buffer_LCD_row_2, "H: %i %c",Hum,37);
+
+    Scale_Temperature=3;
   }
 
   return;
@@ -327,10 +403,10 @@ void send_temperature_RS232(void){
   return;
 }
 
-void show_temperature_LCD(unsigned char *buffer_LCD_row_1, unsigned char *buffer_LCD_row_2, unsigned char Temp_n1, unsigned char Hum_n1){
+void show_temperature_LCD(unsigned char *buffer_LCD_row_1, unsigned char *buffer_LCD_row_2, unsigned char Temp_n1, unsigned char Hum_n1, unsigned char Scale_Temperature_n1){
   unsigned char counter=0;
 
-  if(Temp_n1!=Temp){
+  if(Temp_n1!=Temp || Scale_Temperature!=Scale_Temperature_n1){
     DireccionaLCD(0x80);
     // Write first row
     while (buffer_LCD_row_1[counter]!='\0') {
@@ -365,53 +441,7 @@ void show_temperature_LCD(unsigned char *buffer_LCD_row_1, unsigned char *buffer
   return;
 }
 
-void LeerHT11(void){
-  unsigned char i,contr=0;
-  DATA_DIR=0;
-  __delay_ms(18);
-  DATA_DIR=1;
-  __delay_ms(1);
-  while(DATA_IN==1);
-  __delay_us(40);
-  if(DATA_IN==0) contr++;
-  __delay_us(80);
-  if(DATA_IN==1) contr++;
-  while(DATA_IN==1);
-  Hum=LeerByte();
-  LeerByte();
-  Temp=LeerByte();
-  LeerByte();
-  Che=LeerByte();
-  return;
-}
 
-unsigned char LeerByte(void){
-  unsigned char res=0,i;
-  for(i=8;i>0;i--){
-    res=(res<<1) | LeerBit();  
-  }
-  return res;
-}
-
-unsigned char LeerBit(void){
-  unsigned char res=0;
-  while(DATA_IN==0);
-  __delay_us(13);
-  if(DATA_IN==1) res=0;
-  __delay_us(22);
-  if(DATA_IN==1){
-    res=1;
-    while(DATA_IN==1);
-  }  
-  return res;  
-}
-
-unsigned char Check(void){
-  unsigned char res=0,aux;
-  aux=Temp+Hum;
-  if(aux==Che) res=1;
-  return res;  
-}
 
 void Transmitir(unsigned char BufferT){
   while(TRMT==0);
@@ -429,9 +459,34 @@ void interrupt ISR(void){
   return;
 }
 
+void EEPROM_Write (unsigned char address, unsigned char data){
+  EEADR=address;
+  EEDATA=data;
+ 
+  WREN=1;
+
+  EECON2=0x55;
+  EECON2=0xAA;
+
+  WR=1;
+  while(WR==1);
+
+  WREN=0;
+  return;
+
+}
+
+unsigned char EEPROM_Read (unsigned char address){
+  EEADR=address;
+  RD=1;
+  return EEDATA;
+}
+
 /*
 
   Bibliography
   WDT configuration : // https://www.electronicwings.com/pic/pic18f4550-watchdog-timer
-  
+  EEPROM : https://www.electronicwings.com/pic/pic18f4550-on-chip-eeprom
+  DHT11 : https://www.electronicwings.com/pic/dht11-sensor-interfacing-with-pic18f4550
+  Examples: https://github.com/lcgamboa/picsimlab_examples/tree/master/docs/board_Breadboard/PIC18F4550/test_b0/src/teste_b0.X
  */
